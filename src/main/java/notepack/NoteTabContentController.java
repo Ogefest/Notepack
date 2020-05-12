0b36eb322503c313a0a50704d2e5e170399bf36e
Ogefest <hello@ogefest.com>
2020-05-12 23:27:39 +0200

    search/replace form

--- src/main/java/notepack/NoteTabContentController.java
+++ src/main/java/notepack/NoteTabContentController.java
@@ -5,15 +5,25 @@
  */
 package notepack;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.MenuItem;
 
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import notepack.app.domain.Note;
 
 /**
@@ -82,6 +92,70 @@ public class NoteTabContentController implements Initializable {
 
     @FXML
     private void onSearchInNote(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("SearchForm.fxml"));
+
+        Scene scene;
+        try {
+            Parent root = fxmlLoader.load();
+
+            SearchFormController ctrl = (SearchFormController) fxmlLoader.getController();
+            ctrl.setCallback(new SearchFormCallback() {
+                @Override
+                public void search(String string) {
+                    int caretPost = textArea.getCaretPosition();
+                    int indexStart = textArea.getText().indexOf(string, caretPost);
+                    
+                    if (textArea.getText().indexOf(string) == -1) {
+                        Alert a = new Alert(AlertType.INFORMATION, string + " was not found. Start from beginning?", ButtonType.OK); 
+                        a.showAndWait();
+                    } else {
+                        if (indexStart > 0) {
+                            textArea.selectRange(indexStart, indexStart + string.length());
+                        } else {
+                            indexStart = textArea.getText().indexOf(string, 0);
+                            textArea.selectRange(indexStart, indexStart + string.length());
+                        }
+                    }
+                    
+                }
+
+                @Override
+                public void replace(String from, String to, boolean replaceAll) {
+                    
+                    int caretPost = textArea.getCaretPosition();
+                    
+                    if (replaceAll) {
+                        String current = textArea.getText();
+                        String afterReplacement = current.replace(from, to);
+                        textArea.setText(afterReplacement);
+                    } else {
+                        String taText = textArea.getText();
+                        int indexStart = taText.indexOf(from, caretPost);
+                        if (indexStart > 0) {
+                            
+                            String part1 = taText.substring(0, indexStart);
+                            String part2 = taText.substring(indexStart + from.length());
+                            
+                            String result = part1 + to + part2;
+                            textArea.setText(result);
+                            textArea.positionCaret(indexStart);
+                            
+                        }
+                    }
+                    
+                }
+            });
+
+            scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("Search/Replace");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }         
     }
 
     @FXML
