f9e59f72318ced24ed578bde6ad644a2e2b6fab3
Ogetfest <lukasz@gajos.eu>
2020-06-12 13:06:29 +0200

    logo for stage to show in task bar

--- src/main/java/notepack/Main.java
+++ src/main/java/notepack/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,7 +19,10 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
         loader.setResources(ResourceBundle.getBundle("notepack.fonts.FontAwesome"));
 
-//        App.class.getResource(fxml + ".fxml");
+        Image icon = new Image(getClass().getResourceAsStream("logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Notepack");
+
         Parent root = loader.load();
 
         MainViewController ctrl = loader.getController();
