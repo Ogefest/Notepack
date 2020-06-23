3b04496122278ee4fc8cb3856cd9c874ca043f3d
Ogefest <lukasz@gajos.eu>
2020-06-23 22:53:35 +0200

    middleware to add some preprocessors and postprocessor for note

--- src/main/java/notepack/app/domain/NoteStorageMiddleware.java
+++ src/main/java/notepack/app/domain/NoteStorageMiddleware.java
@@ -1,39 +1,37 @@
 package notepack.app.domain;
 
 import java.util.ArrayList;
-import notepack.app.domain.NoteStorageConfiguration;
-import notepack.app.domain.NoteStorageItem;
+import notepack.app.domain.exception.MessageError;
 import notepack.processor.NoteProcessor;
 
-
 public class NoteStorageMiddleware implements NoteStorage {
-    
+
     private NoteStorage noteStorage;
     private ArrayList<NoteProcessor> beforeSave = new ArrayList<>();
     private ArrayList<NoteProcessor> afterSave = new ArrayList<>();
     private ArrayList<NoteProcessor> beforeLoad = new ArrayList<>();
     private ArrayList<NoteProcessor> afterLoad = new ArrayList<>();
-    
+
     public NoteStorageMiddleware(NoteStorage noteStorage) {
         this.noteStorage = noteStorage;
     }
-    
+
     public void registerBeforeSave(NoteProcessor p) {
         beforeSave.add(p);
     }
-    
+
     public void registerAfterSave(NoteProcessor p) {
         afterSave.add(p);
     }
-    
+
     public void registerBeforeLoad(NoteProcessor p) {
         beforeLoad.add(p);
     }
-    
+
     public void registerAfterLoad(NoteProcessor p) {
         afterLoad.add(p);
     }
-    
+
     public NoteStorage getNoteStorage() {
         return noteStorage;
     }
@@ -49,27 +47,27 @@ public class NoteStorageMiddleware implements NoteStorage {
     }
 
     @Override
-    public void saveContent(String content, String path) {
-        
+    public void saveContent(String content, String path) throws MessageError {
+
         for (NoteProcessor p : beforeSave) {
             content = p.run(content);
         }
-        
+
         noteStorage.saveContent(content, path);
-        
+
         for (NoteProcessor p : afterSave) {
             content = p.run(content);
         }
     }
 
     @Override
-    public String loadContent(String path) {
+    public String loadContent(String path) throws MessageError {
         String content = noteStorage.loadContent(path);
-        
+
         for (NoteProcessor p : afterLoad) {
             content = p.run(content);
         }
-        
+
         return content;
     }
 
