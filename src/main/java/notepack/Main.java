32c1cbed5cb48ea289d608916f7e0cf7313c689a
Ogefest <lukasz@gajos.eu>
2020-06-23 22:40:34 +0200

    bouncycastle register

--- src/main/java/notepack/Main.java
+++ src/main/java/notepack/Main.java
@@ -1,5 +1,6 @@
 package notepack;
 
+import java.security.Security;
 import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main extends Application {
 
@@ -16,6 +18,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Security.addProvider(new BouncyCastleProvider());
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
         loader.setResources(ResourceBundle.getBundle("notepack.fonts.FontAwesome"));
 
