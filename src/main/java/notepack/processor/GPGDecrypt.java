d9e0133ae28a0bc295efc4cd852693104809af88
Ogefest <lukasz@gajos.eu>
2020-06-23 21:57:19 +0200

    GPG preprocessor and postprocessor

--- src/main/java/notepack/processor/GPGDecrypt.java
+++ src/main/java/notepack/processor/GPGDecrypt.java
@@ -1,17 +1,20 @@
 package notepack.processor;
 
+import notepack.app.domain.exception.MessageError;
 import notepack.encrypt.GPG;
 
 
 public class GPGDecrypt implements NoteProcessor {
+    
+    private GPG gpg;
 
     public GPGDecrypt(GPG gpg) {
-        
+        this.gpg = gpg;
     }
     
     @Override
-    public String run(String input) {
-        return input;
+    public String run(String input) throws MessageError {
+        return gpg.decrypt(input);
     }
 
 }
--- src/main/java/notepack/processor/NoteProcessor.java
+++ src/main/java/notepack/processor/NoteProcessor.java
@@ -1,6 +1,8 @@
 package notepack.processor;
 
+import notepack.app.domain.exception.MessageError;
 
 public interface NoteProcessor {
-    public String run(String input);
+
+    public String run(String input) throws MessageError;
 }
