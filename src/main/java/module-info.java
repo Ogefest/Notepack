80d90d348af66c50b5a639cc1f5024e6eafc584f
Ogefest <lukasz@gajos.eu>
2020-06-23 23:40:39 +0200

    dependency changed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,8 @@ module notepack {
     requires java.prefs;
     requires org.json;
     requires java.base;
-    requires org.bouncycastle.pg;
-    requires org.bouncycastle.provider;
+//    requires org.bouncycastle.pg;
+//    requires org.bouncycastle.provider;
+    requires bcprov.jdk15on;
+    requires bcpg.jdk15on;
 }
