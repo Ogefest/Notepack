8b25b09238cfcf325914b58c0deb0933d2c0b861
Ogefest <lukasz@gajos.eu>
2020-06-23 22:50:17 +0200

    close note in session when opening and something goes wrong

--- src/main/java/notepack/app/task/OpenNote.java
+++ src/main/java/notepack/app/task/OpenNote.java
@@ -2,24 +2,31 @@ package notepack.app.task;
 
 import notepack.app.domain.Note;
 import notepack.app.domain.Task;
+import notepack.app.domain.exception.MessageError;
 import notepack.app.listener.NoteListener;
 
-public class OpenNote implements Task,TypeNote {
-    
+public class OpenNote extends BaseTask implements Task, TypeNote {
+
     private Note note;
-    
+
     public OpenNote(Note note) {
         this.note = note;
     }
 
     @Override
-    public void dispatch() {
-        note.readFromStorage();
+    public void dispatch() throws MessageError {
+        try {
+            note.readFromStorage();
+        } catch(MessageError ex) {
+            messageBus.addTask(new CloseNote(note));
+            
+            throw ex;
+        }
     }
 
     @Override
     public void notify(NoteListener listener) {
         listener.onOpen(note);
     }
-    
+
 }
