4074c151a244a3f4c748da38ffb74019e652345c
Ogefest <lukasz@gajos.eu>
2020-08-04 23:00:26 +0200

    fix GPG show password dialog

--- src/main/java/notepack/app/task/BaseTask.java
+++ src/main/java/notepack/app/task/BaseTask.java
@@ -1,12 +1,24 @@
 package notepack.app.task;
 
 import notepack.app.domain.MessageBus;
+import notepack.app.domain.Task;
 
 abstract public class BaseTask {
 
     protected MessageBus messageBus;
+    protected Task afterDispatchTask;
 
     public void setMessageBus(MessageBus mb) {
         this.messageBus = mb;
+    }
+
+    public void setAfterDispatchTask(Task t) {
+        afterDispatchTask = t;
+    }
+
+    protected void runNextTaskInChain() {
+        if (messageBus != null && afterDispatchTask != null) {
+            messageBus.addTask(afterDispatchTask);
+        }
     }
 }
