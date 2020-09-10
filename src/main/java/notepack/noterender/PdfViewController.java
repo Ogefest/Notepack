2f1086c8ec3481caa0c409c6f1676044db658ca9
lgajos <lukasz@transparentdata.pl>
2020-09-10 23:42:47 +0200

    pdf zoom

--- src/main/java/notepack/noterender/PdfViewController.java
+++ src/main/java/notepack/noterender/PdfViewController.java
@@ -6,10 +6,14 @@ import java.util.Base64;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
@@ -33,6 +37,12 @@ public class PdfViewController implements Initializable, NoteRenderController {
     private WebView webView;
     
     private WebEngine webEngine;
+    @FXML
+    private TextField pageNumber;
+    @FXML
+    private Label labelMaxPage;
+    @FXML
+    private ComboBox<String> zoomSelect;
 
     /**
      * Initializes the controller class.
@@ -46,6 +56,14 @@ public class PdfViewController implements Initializable, NoteRenderController {
         String viewerUrl = getClass().getResource("/notepack/noterender/pdfjs/web/viewer.html").toExternalForm();
         webEngine.load(viewerUrl);
         
+        ObservableList<String> list = zoomSelect.getItems();
+        list.add("Auto");
+        list.add("Page fit");
+        list.add("Page width");
+        list.add("25%");
+        list.add("50%");
+        list.add("100%");
+        list.add("200%");
     }
     
     @FXML
@@ -75,6 +93,9 @@ public class PdfViewController implements Initializable, NoteRenderController {
                         
                         webEngine.executeScript("console.log = function(message){ app.log(message); };");
                         webEngine.executeScript("window.loadPdf()");
+                        
+//                        JSObject jsobj = (JSObject) webEngine.executeScript("PDFViewerApplication.pdfViewer");
+//                        labelMaxPage.setText("of " + jsobj.toString());
                     }
                     
                 });
@@ -87,6 +108,54 @@ public class PdfViewController implements Initializable, NoteRenderController {
 
     @Override
     public void noteDeactivated() {
+    }
+
+    @FXML
+    private void onPrevPage(ActionEvent event) {
+        webEngine.executeScript("PDFViewerApplication.pdfViewer.currentPageNumber -= 1");
+    }
+
+    @FXML
+    private void onNextPage(ActionEvent event) {
+        webEngine.executeScript("PDFViewerApplication.pdfViewer.currentPageNumber += 1");
+    }
+
+    @FXML
+    private void onZoomChanged(ActionEvent event) {
+        String currentZoom = zoomSelect.getValue();
+        
+        String cmd = "auto";
+        
+        switch (currentZoom) {
+            case "Auto":
+                cmd = "auto";
+                break;
+            case "Page fit":
+                cmd = "page-fit";
+                break;
+            case "Page width":
+                cmd = "page-width";
+                break;
+            case "25%":
+                cmd = "0.25";
+                break;
+            case "50%":
+                cmd = "0.5";
+                break;
+            case "75%":
+                cmd = "0.75";
+                break;
+            case "100%":
+                cmd = "1";
+                break;
+            case "200%":
+                cmd = "2";
+                break;
+            default:
+                cmd = "auto";
+        }
+        
+        webEngine.executeScript("PDFViewerApplication.pdfViewer._setScale('"+cmd+"')");
     }
     
 }
