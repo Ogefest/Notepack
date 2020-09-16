4665fff3a93f2f9706a6a7957b56eb91b9b25ae4
lgajos <lukasz@transparentdata.pl>
2020-09-16 23:51:31 +0200

    pdf view css

--- src/main/java/notepack/noterender/PdfViewJsCallback.java
+++ src/main/java/notepack/noterender/PdfViewJsCallback.java
@@ -4,19 +4,31 @@ import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class PdfViewJsCallback {
-        private byte[] pdfContent;
 
-        public PdfViewJsCallback(byte[] content) {
-            this.pdfContent = content;
+    private byte[] pdfContent;
+    private int currentPage = 0;
+    private PdfViewJsPageTracker tracker;
+
+    public PdfViewJsCallback(byte[] content, PdfViewJsPageTracker tracker) {
+        this.pdfContent = content;
+        this.tracker = tracker;
+    }
+
+    public String getBase64Content() {
+        return Base64.getEncoder().encodeToString(pdfContent);
+    }
+
+    public void log(String message) {
+        Logger.getLogger(PdfViewController.class.getName()).log(Level.SEVERE, message);
+    }
+
+    public void setCurrentPageInfo(int currentPage, int maxPage) {
+        if (this.currentPage == currentPage) {
+            return;
         }
 
-        public String getBase64Content() {
-            return Base64.getEncoder().encodeToString(pdfContent);
-        }
-        
-        public void log(String message) {
-            Logger.getLogger(PdfViewController.class.getName()).log(Level.SEVERE, message);
-        }
+        this.currentPage = currentPage;
+        tracker.pageChanged(currentPage, maxPage);
+    }
 }
