29a8063dbece131723e57de4b7eaa75f4a90f952
lgajos <lukasz@transparentdata.pl>
2020-10-20 13:54:09 +0200

    flexmark is not necessary

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -20,16 +20,5 @@ module notepack {
     requires java.base;
     requires org.bouncycastle.pg;
     requires org.bouncycastle.provider;
-    requires flexmark;
-    
-    requires flexmark.ext.abbreviation;
-    requires flexmark.ext.autolink;
-    requires flexmark.ext.definition;
-    requires flexmark.ext.gfm.strikethrough;
-    requires flexmark.ext.tables;
-    requires flexmark.ext.typographic;
-    requires flexmark.util.ast;
-    requires flexmark.util.data;
-    requires flexmark.util.misc;
 
 }
