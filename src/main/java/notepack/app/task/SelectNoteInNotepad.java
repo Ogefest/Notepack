df85706d38ab38ff2a9d97c5c51aee68ee54c7e2
Ogefest <hello@ogefest.com>
2021-01-02 11:02:27 +0100

    check if notepad tab is notepad instance

--- src/main/java/notepack/app/task/SelectNoteInNotepad.java
+++ src/main/java/notepack/app/task/SelectNoteInNotepad.java
@@ -8,6 +8,7 @@ import notepack.app.domain.App;
 import notepack.app.domain.Note;
 import notepack.app.domain.Notepad;
 import notepack.app.domain.Task;
+import notepack.gui.TabNotepad;
 
 public class SelectNoteInNotepad implements Task, TypeGui {
 
@@ -30,13 +31,15 @@ public class SelectNoteInNotepad implements Task, TypeGui {
         TabPane container = (TabPane) stage.getScene().lookup("#notepadTabContainer");
         Notepad notepad = note.getNotepad();
         for (Tab tab : container.getTabs()) {
-            NotebookTabController ctrl = (NotebookTabController) tab.getUserData();
-            if (ctrl.getNotepad().getIdent().equals(notepad.getIdent())) {
-                container.getSelectionModel().select(tab);
+            if (tab instanceof TabNotepad) {
+                NotebookTabController ctrl = (NotebookTabController) tab.getUserData();
+                if (ctrl.getNotepad().getIdent().equals(notepad.getIdent())) {
+                    container.getSelectionModel().select(tab);
 
-                ctrl.selectNote(note);
+                    ctrl.selectNote(note);
 
-                break;
+                    break;
+                }
             }
         }
     }
