931bbac784d88f2766f1a75d2bdf8f5db83a934c
lgajos <lukasz@transparentdata.pl>
2021-01-17 19:59:08 +0100

    store object in meta

--- src/main/java/notepack/app/domain/NoteMetaStorage.java
+++ src/main/java/notepack/app/domain/NoteMetaStorage.java
@@ -1,6 +1,7 @@
 package notepack.app.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface NoteMetaStorage {
     public void changeNamespace(String old, String current);
@@ -8,4 +9,7 @@ public interface NoteMetaStorage {
     public void setValue(String key, String value);
     public ArrayList<String> getArray(String key);
     public void setArray(String key, ArrayList<String> value);
+
+    public void setObject(String key, HashMap<String, String> map);
+    public HashMap<String, String> getObject(String key);
 }
