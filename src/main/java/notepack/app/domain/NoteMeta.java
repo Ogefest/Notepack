c6782fac0a3ff6f0f2f49ffef68da0b8c10ab8df
Ogefest <hello@ogefest.com>
2021-01-31 15:24:01 +0100

    code cleanup

--- src/main/java/notepack/app/domain/NoteMeta.java
+++ src/main/java/notepack/app/domain/NoteMeta.java
@@ -36,41 +36,41 @@ public class NoteMeta {
         storage.setArray("tag", tags);
     }
 
-    public void setTodo(Todo todo) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("duedate", todo.getDueDate().toString());
-        map.put("summary", todo.getSummary());
-        map.put("finished", todo.isFinished() ? "1" : "0");
+//    public void setTodo(Todo todo) {
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("duedate", todo.getDueDate().toString());
+//        map.put("summary", todo.getSummary());
+//        map.put("finished", todo.isFinished() ? "1" : "0");
+//
+//        storage.setObject("todo", map);
+//    }
 
-        storage.setObject("todo", map);
-    }
-
-    public void removeTodo() {
-        storage.setObject("todo", null);
-    }
-
-    public Todo getTodo() {
-
-        HashMap<String, String> map = storage.getObject("todo");
-
-        if (map == null) {
-            return null;
-        }
-
-        if (!map.containsKey("summary")) {
-            return null;
-        }
-
-        LocalDate dueDate = LocalDate.parse(map.get("duedate"));
-        boolean isFinished = map.get("finished").equals("1") ? true : false;
-        String summary = map.get("summary");
-
-        Todo result = new Todo();
-        result.setDueDate(dueDate);
-        result.setFinished(isFinished);
-        result.setSummary(summary);
-
-        return result;
-    }
+//    public void removeTodo() {
+//        storage.setObject("todo", null);
+//    }
+//
+//    public Todo getTodo() {
+//
+//        HashMap<String, String> map = storage.getObject("todo");
+//
+//        if (map == null) {
+//            return null;
+//        }
+//
+//        if (!map.containsKey("summary")) {
+//            return null;
+//        }
+//
+//        LocalDate dueDate = LocalDate.parse(map.get("duedate"));
+//        boolean isFinished = map.get("finished").equals("1") ? true : false;
+//        String summary = map.get("summary");
+//
+//        Todo result = new Todo();
+//        result.setDueDate(dueDate);
+//        result.setFinished(isFinished);
+//        result.setSummary(summary);
+//
+//        return result;
+//    }
 
 }
